748bcdd4cef163eaf6f33c5096bc06002d169f30
gz65555 <gz65555@gmail.com>
2022-12-07 23:37:11 +0800

    feat: init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import schedule from "node-schedule";
+const date = new Date();
+date.setSeconds(date.getSeconds() + 20);
+
+const job = schedule.scheduleJob(date, function () {
+  console.log("The world is going to end today.");
+});
+
+console.log("job start");
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: "John Doe" });
 }
